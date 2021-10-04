6d6008757c6092b39343b24b7bcd572c2935216b
AatroXiss <antoine.beaudesson@gmail.com>
2021-10-04 11:45:45 +0200

    v.0.0.2 upgraded static version of HP

---
+++ js/card_slider.js
@@ -0,0 +1,18 @@
+// Card Sliders for Just Stream it
+
+let cardContainers = [...document.querySelectorAll('.card_container')];
+let preBtns = [...document.querySelectorAll('.pre_btn')];
+let nextBtns = [...document.querySelectorAll('.next_btn')];
+
+cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nextBtns[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth - 200;
+    })
+
+    preBtns[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
